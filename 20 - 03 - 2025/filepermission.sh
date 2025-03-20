54f2cb9a14c9f52fb901ec24ee9ef21ea4fa7a69
jasirkonnola <jasirhassan.894315@gmail.com>
2025-03-20 11:37:30 +0530

    Update filepermission.sh

--- 20 - 03 - 2025/filepermission.sh
+++ 20 - 03 - 2025/filepermission.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 read -e -p "Enter the filename :" filename
-if [[ -r $filename && -w $filename && -x $filenmae ]]; then
-	echo "the file have read,write,execute permission."
-elif [[ -r $filename && -w $filename ]]; then
-	echo "the file have read,write permission."
+if [[ -r $filename ]]; then
+        echo "the file have read permission."
 else
-	echo "no permission"
+        echo "the file have no read permission."
 fi
+
+if [[ -w $filename ]]; then
+        echo "the file have write permission."
+else
+        echo "the file have no write permission."
+fi
+if [[ -x $filename ]]; then
+        echo "the file have execute permission."
+else
+        echo "the file have no execute permission."
+fi
+
